b9a0df96116553fb197a2f144ead7d8453efc678
gulamekhwaja1999 <gulamekhwaja5@gmail.com>
2026-02-12 22:43:24 +0530

    one mor file

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ class Main{
     {
          System.out.println("Hello jenkis");
          System.out.println("My name is gulame khwaja");
+
+         Student std=new Student();
+         std.sayhello();
+         
     }
 }
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public Student()
+    {
+        System.out.println(".Student object");
+    }
+    public void sayhello()
+    {
+        System.out.println(" Hi i am Student");
+    }
+}
